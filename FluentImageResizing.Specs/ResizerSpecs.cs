5f924bf9fc0af28b65e726cdac1565ad59a5669c
Marius Schulz <marius.schulz@me.com>
2012-09-21 22:20:08 +0200

    Extracted base class form unit test classes

--- FluentImageResizing.Specs/ResizerSpecs.cs
+++ FluentImageResizing.Specs/ResizerSpecs.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using NUnit.Framework;
 using Should.Fluent;
 
@@ -9,21 +7,12 @@ namespace FluentImageResizing.Specs
 {
     #region When an image is read by the Resizer class
 
-    [TestFixture]
-    public class When_an_image_is_read_by_the_Resizer_class
+    public class When_an_image_is_read_by_the_Resizer_class : TestFixtureWorkingWithImages
     {
-        private byte[] _imageBytes;
-
-        [TestFixtureSetUp]
-        public void SetUp()
-        {
-            _imageBytes = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "../../image.png"));
-        }
-
         [Test]
         public void The_image_data_is_stored_correctly()
         {
-            Resizer.CreateImageFrom(_imageBytes, ImageFormat.Png).Bytes.Should().Equal(_imageBytes);
+            Resizer.CreateImageFrom(ImageBytes, ImageFormat.Png).Bytes.Should().Equal(ImageBytes);
         }
     }
 
@@ -31,24 +20,13 @@ namespace FluentImageResizing.Specs
 
     #region When an image is cropped to the specified maximum dimensions
 
-    [TestFixture]
-    public class When_an_image_is_cropped_to_the_specified_maximum_dimensions
+    public class When_an_image_is_cropped_to_the_specified_maximum_dimensions : TestFixtureWorkingWithImages
     {
-        private byte[] _imageBytes;
-        private byte[] _iconBytes;
-
-        [TestFixtureSetUp]
-        public void SetUp()
-        {
-            _imageBytes = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "../../image.png"));
-            _iconBytes = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "../../icon.png"));
-        }
-
         [Test]
         public void The_dimensions_of_the_resulting_image_equal_the_specified_dimensions_if_the_image_dimensions_are_bigger_than_the_specified_ones()
         {
             Image image = Resizer
-                 .CreateImageFrom(_imageBytes, ImageFormat.Png)
+                 .CreateImageFrom(ImageBytes, ImageFormat.Png)
                  .Crop.FromCenter.ToAtMost(75, 60)
                  .CreateImage();
 
@@ -60,7 +38,7 @@ namespace FluentImageResizing.Specs
         public void The_dimensions_of_the_resulting_image_equal_the_image_dimensions_if_the_image_dimensions_are_smaller_than_the_specified_ones()
         {
             Image image = Resizer
-                .CreateImageFrom(_iconBytes, ImageFormat.Png)
+                .CreateImageFrom(IconBytes, ImageFormat.Png)
                 .Crop.FromCenter.ToAtMost(75, 60)
                 .CreateImage();
 
---
+++ FluentImageResizing.Specs/TestFixtureWorkingWithImages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace FluentImageResizing.Specs
+{
+    [TestFixture]
+    public abstract class TestFixtureWorkingWithImages
+    {
+        protected byte[] ImageBytes;
+        protected byte[] IconBytes;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            ImageBytes = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "../../image.png"));
+            IconBytes = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "../../icon.png"));
+        }
+    }
+}
