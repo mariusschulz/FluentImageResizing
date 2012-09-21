f63dab78304dbf3f201c16db90a22d5a3039b710
Marius Schulz <marius.schulz@me.com>
2012-09-21 23:09:37 +0200

    Fixed problem with indexed images

--- FluentImageResizing/Resizer.cs
+++ FluentImageResizing/Resizer.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing.Imaging;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace FluentImageResizing
 {
@@ -6,7 +8,27 @@ namespace FluentImageResizing
     {
         public static ImageResult CreateImageFrom(byte[] bytes, ImageFormat format)
         {
-            return new ImageResult(bytes, format);
+            byte[] nonIndexedImageBytes;
+
+            using (var memoryStream = new MemoryStream(bytes))
+            using (var image = Image.FromStream(memoryStream))
+            {
+                var temporaryBitmap = new Bitmap(image.Width, image.Height);
+
+                using (Graphics grPhoto = Graphics.FromImage(temporaryBitmap))
+                {
+                    grPhoto.DrawImage(temporaryBitmap, new Rectangle(0, 0, temporaryBitmap.Width, temporaryBitmap.Height),
+                        0, 0, temporaryBitmap.Width, temporaryBitmap.Height, GraphicsUnit.Pixel);
+                }
+               
+                using (var saveStream = new MemoryStream())
+                {
+                    temporaryBitmap.Save(saveStream, format);
+                    nonIndexedImageBytes = saveStream.ToArray();
+                } 
+            }
+
+            return new ImageResult(nonIndexedImageBytes, format);
         }
     }
 }
