f495c27c574170f5bcd412bf97ecafbc182d5c31
Marius Schulz <marius.schulz@me.com>
2012-09-21 21:59:48 +0200

    Added tests for images smaller than the crop

--- FluentImageResizing.Specs/The_Resizer_class.cs
+++ FluentImageResizing.Specs/The_Resizer_class.cs
@@ -12,12 +12,13 @@ namespace FluentImageResizing.Specs
     public class The_Resizer_class
     {
         private byte[] _imageBytes;
+        private byte[] _iconBytes;
 
         [TestFixtureSetUp]
         public void SetUp()
         {
-            string imagePath = Path.Combine(Environment.CurrentDirectory, "../../image.png");
-            _imageBytes = File.ReadAllBytes(imagePath);
+            _imageBytes = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "../../image.png"));
+            _iconBytes = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "../../icon.png"));
         }
 
         [Test]
@@ -38,6 +39,18 @@ namespace FluentImageResizing.Specs
 
             image.Width.Should().Equal(75);
             image.Height.Should().Equal(60);
+        }
+
+        [Test]
+        public void Uses_the_image_size_if_the_image_to_crop_is_smaller_than_the_crop_size()
+        {
+            Image image = Resizer
+                .CreateImageFrom(_iconBytes, ImageFormat.Png)
+                .Crop.FromCenter.ToAtMost(75, 60)
+                .CreateImage();
+
+            image.Width.Should().Equal(16);
+            image.Height.Should().Equal(16);
         }
     }
 }
