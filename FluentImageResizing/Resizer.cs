c3a438023ea588ade108aaa0cebbffd84efe6da5
Marius Schulz <marius.schulz@me.com>
2013-02-21 16:08:35 +0100

    Added missing using statement

--- FluentImageResizing/Resizer.cs
+++ FluentImageResizing/Resizer.cs
@@ -13,19 +13,20 @@ namespace FluentImageResizing
             using (var memoryStream = new MemoryStream(bytes))
             using (var image = Image.FromStream(memoryStream))
             {
-                var temporaryBitmap = new Bitmap(image.Width, image.Height);
+                using (var temporaryBitmap = new Bitmap(image.Width, image.Height))
+                {
+                    using (var graphics = Graphics.FromImage(temporaryBitmap))
+                    {
+                        graphics.DrawImage(image, new Rectangle(0, 0, temporaryBitmap.Width, temporaryBitmap.Height),
+                            0, 0, temporaryBitmap.Width, temporaryBitmap.Height, GraphicsUnit.Pixel);
+                    }
 
-                using (var graphics = Graphics.FromImage(temporaryBitmap))
-                {
-                    graphics.DrawImage(image, new Rectangle(0, 0, temporaryBitmap.Width, temporaryBitmap.Height),
-                        0, 0, temporaryBitmap.Width, temporaryBitmap.Height, GraphicsUnit.Pixel);
+                    using (var saveStream = new MemoryStream())
+                    {
+                        temporaryBitmap.Save(saveStream, format);
+                        nonIndexedImageBytes = saveStream.ToArray();
+                    }
                 }
-               
-                using (var saveStream = new MemoryStream())
-                {
-                    temporaryBitmap.Save(saveStream, format);
-                    nonIndexedImageBytes = saveStream.ToArray();
-                } 
             }
 
             return new ImageResult(nonIndexedImageBytes, format);
